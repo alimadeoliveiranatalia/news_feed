2b6c1c97ccc610fbf6c2c1cbe83004d78c767c2b
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-02-23 13:55:53 -0300

    feat: add header component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import "./global.css";
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <h3>Setup Blog</h3>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+﻿import styles from "./Header.module.css";
+import astronautaLogo from "../assets/astronauta.svg";
+
+export function Header(){
+    return (
+        <header className={styles.header}>
+            <img src={astronautaLogo} alt="Logo de Astronautas" />
+            <strong>Feed</strong>
+        </header>
+    )
+}
