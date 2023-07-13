c44b22212d8e829a26c0c383eed2baacae80f1eb
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-07-13 15:19:24 -0300

    refactor: add url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/news_feed/",
+  base: "https://github.com/alimadeoliveiranatalia/news_feed/",
   plugins: [react()],
 })
